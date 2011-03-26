8925bda77c666cc0a440788fdbe8d712c61457ca
Geoffrey Grosenbach <boss@topfunky.com>
2011-03-25 17:30:36 -0700

    Use project root directory as project root (or file if not found).

--- PeepOpen-tmplugin/PeepOpen.m
+++ PeepOpen-tmplugin/PeepOpen.m
@@ -28,29 +28,28 @@ static PeepOpen *po;
 - (void)goToFile:(id)sender
 {
 	NSString *projectDir;
-	// If there is no window open we need to find the open window and extract
-	// the projectDirectory from that
-	if([[currentDocument valueForKey:@"filename"] length] == 0)
-	{
-		OakProjectController *project = NULL;
-		for (NSWindow *w in [[NSApplication sharedApplication] orderedWindows]) {
-			if ([[[w windowController] className] isEqualToString: @"OakProjectController"] &&
+  OakProjectController *project = NULL;
+
+  // First try to find a window with a project and get the projectDirectory
+  // from it.
+  for (NSWindow *w in [[NSApplication sharedApplication] orderedWindows]) {
+    if ([[[w windowController] className] isEqualToString: @"OakProjectController"] &&
 				[[w windowController] projectDirectory]) {
-				project = [w windowController];
-				break;
-			}
-		}
-		if (project != NULL) {
-			projectDir =  [project projectDirectory];
-		}
-	}
-	else
-	{
+      project = [w windowController];
+      break;
+    }
+  }
+
+  if (project != NULL) {
+    projectDir =  [project projectDirectory];
+  } 
+  else if ([[currentDocument valueForKey:@"filename"] length] > 0) {
 		projectDir = [currentDocument valueForKey:@"filename"];
-	}
+  }
+  
 	NSString *projectURLString = [NSString stringWithFormat:@"peepopen://%@?editor=TextMate", projectDir];
 	NSURL *url = [NSURL URLWithString:projectURLString];
-	NSLog(@"OakprojectController (PeepOpen), sending url %@ to NSWorkspace", [url absoluteString]);
+	NSLog(@"PeepOpen: Opening URL %@", [url absoluteString]);
 	[[NSWorkspace sharedWorkspace] openURL:url];
 }
 @end
